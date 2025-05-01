8d6534986cb6f8f2ad9fc2666ede67bc733b952c
sampath chowdary <96104961+sampath112@users.noreply.github.com>
2025-05-01 15:44:31 +0530

    Update professional.model.js

--- Mindsye_final_code/DSI_App_Backend_now/DSI_App_Backend/models/professional.model.js
+++ Mindsye_final_code/DSI_App_Backend_now/DSI_App_Backend/models/professional.model.js
@@ -1,51 +1,27 @@
-// Copyright 2024 Umang Patel
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     https://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Copyright 2024 Umang Patel
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     https://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 import mongoose from 'mongoose';
 
 const ProfessionalSchema = new mongoose.Schema({
-    name : {
+    name: {
         type: String,
         required: true
     },
-    Number : {
+    Number: {
         type: String,
         required: true
     },
-    Address : {
+    Address: {
         type: String,
         required: true
     },
-    ProfessionalID : {
+    ProfessionalID: {
         type: String,
         required: true
     },
+    assignedSchools: {
+        type: [String],
+        default: [],
+      },
 }, { timestamps: true });
 
-// export const Teacher = mongoose.model('Teacher', TeacherSchema);
 export const Professional = mongoose.model('Professional', ProfessionalSchema);
